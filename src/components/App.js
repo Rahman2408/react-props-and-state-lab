42e93da9eafea1ca92b1200fadca225047f5fefa
Rahman Elsirafy <rahman2408@gmail.com>
2021-12-13 05:02:59 -0500

    commit tree 984f149e2e7c92cf89fb60b52f1c6a70229cd69e

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,34 @@ class App extends React.Component {
     }
   }
 
+  onFindPetsClick = () => {
+    let endApi = '/api/pets'
+
+    if (this.state.filters.type !== 'all') {
+      endApi += `?type=${this.state.filters.type}`
+    }
+    fetch(endApi)
+      .then(res => res.json())
+      .then(pets => this.setState({pets: pets}))
+  }
+
+
+  onChangeType = ({target: {value} }) => {
+    this.setState({
+      filters: {
+        ...this.state.filters,
+        type: value
+      }
+    })
+  }
+
+  onAdoptPet = (petId) => {
+   const pets = this.state.pets.map(pet => {
+     return pet.id === petId ? { ...pet, isAdopted: true} : pet
+    })
+    this.setState({pets: pets})
+  }
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +52,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType} onFindPetsClick={this.onFindPetsClick}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
